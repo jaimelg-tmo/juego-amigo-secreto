c3ff6756016a2487e3f91d23da4a7c92843bdabb
Jaime Lira <jliragarcia.tm@gmail.com>
2025-03-14 13:35:21 -0300

    Se declaran las variables globales

--- appChallenge.js
+++ appChallenge.js
@@ -1,3 +1,9 @@
 //  Challenge Amigo Secreto.
 //  El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
+//  1.  Creo las variables necesarias para almacenar los datos de los amigos y las asignaciones de amigos secretos.
+let amigos = []; // lista de amigos.
+let nombreParticipante = []; // lista de participantes que han descubierto a su amigo secreto.
+let inputParticipante = document.getElementById('participantes');
+let asignaciones = []; // asignaciones de amigos secretos.
+
