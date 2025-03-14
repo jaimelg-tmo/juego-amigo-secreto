5efd390c4ede45e35e7d89945b5f2be3fa5bd81b
Jaime Lira <jliragarcia.tm@gmail.com>
2025-03-14 13:36:32 -0300

    Se crea funcion para agregar participantes

--- appChallenge.js
+++ appChallenge.js
@@ -7,3 +7,30 @@ let nombreParticipante = []; // lista de participantes que han descubierto a su 
 let inputParticipante = document.getElementById('participantes');
 let asignaciones = []; // asignaciones de amigos secretos.
 
+//  2.  Creo una función para agregar los participantes a la lista de amigos.
+function agregarAmigo() {
+    let inputAmigoNuevo = document.getElementById('amigo');
+    let amigoNuevo = inputAmigoNuevo.value.toUpperCase();
+
+    if (!amigoNuevo) {
+        alert('Por favor, escriba un nombre para añadir a la lista de amigos.');
+        inputAmigoNuevo.focus();
+        return;
+    } else {
+        if (amigos.includes(amigoNuevo)) {
+            alert('Este nombre ya ha sido añadido a la lista de amigos.');
+            inputAmigoNuevo.value = '';
+            inputAmigoNuevo.focus();
+            return;
+        } else {
+            amigos.push(amigoNuevo);
+            console.log(amigos);
+            inputAmigoNuevo.value = '';
+            inputAmigoNuevo.focus();
+            mostrarListaAmigos();
+            document.getElementById('borrar').disabled = false;
+            document.getElementById('confirmar').disabled = false;
+            return;
+        }
+    }
+}
